1949154006071520f94839133280ae87c5c961b3
TheLogon <kirilld539@gmail.com>
2022-08-17 17:23:52 +0500

    intro

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,20 @@
 // import { isMobile } from "./functions.js";
 // import { formsModules } from "./forms/forms.js";
 
+const btns = document.querySelectorAll(".magnetic")
+
+btns.forEach(btn => {
+	btn.addEventListener("mousemove", e => {
+		const position = btn.getBoundingClientRect()
+		const x = e.pageX - position.left - position.width
+		const y = e.pageY - position.top - position.height
+
+		btn.style.transform = "translate(" + x * 0.3 + "px, " + y * 0.5 + "px)"
+	})
+})
+
+btns.forEach(btn => {
+	btn.addEventListener("mouseout", e => {
+		btn.style.transform = "translate(0px, 0px)"
+	})
+})
