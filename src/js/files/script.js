fe8385da56f0508ba48aee1c99e84cbba253bd2d
TheLogon <demin0524@gmail.com>
2022-08-25 10:55:33 +0500

    last commit

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,20 +2,43 @@
 // import { isMobile } from "./functions.js";
 // import { formsModules } from "./forms/forms.js";
 
-const btns = document.querySelectorAll(".magnetic")
+$(".custom-select").each(function () {
+	var classes = $(this).attr("class"),
+		id = $(this).attr("id"),
+		name = $(this).attr("name")
+	var template = '<div class="' + classes + '">'
+	template += '<span class="custom-select-trigger">' + $(this).attr("placeholder") + "</span>"
+	template += '<div class="custom-options">'
+	$(this)
+		.find("option")
+		.each(function () {
+			template += '<span class="custom-option ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + "</span>"
+		})
+	template += "</div></div>"
 
-btns.forEach(btn => {
-	btn.addEventListener("mousemove", e => {
-		const position = btn.getBoundingClientRect()
-		const x = e.pageX - position.left - position.width
-		const y = e.pageY - position.top - position.height
-
-		btn.style.transform = "translate(" + x * 0.3 + "px, " + y * 0.5 + "px)"
-	})
+	$(this).wrap('<div class="custom-select-wrapper"></div>')
+	$(this).hide()
+	$(this).after(template)
 })
-
-btns.forEach(btn => {
-	btn.addEventListener("mouseout", e => {
-		btn.style.transform = "translate(0px, 0px)"
+$(".custom-option:first-of-type").hover(
+	function () {
+		$(this).parents(".custom-options").addClass("option-hover")
+	},
+	function () {
+		$(this).parents(".custom-options").removeClass("option-hover")
+	}
+)
+$(".custom-select-trigger").on("click", function () {
+	$("html").one("click", function () {
+		$(".custom-select").removeClass("opened")
 	})
+	$(this).parents(".custom-select").toggleClass("opened")
+	event.stopPropagation()
+})
+$(".custom-option").on("click", function () {
+	$(this).parents(".custom-select-wrapper").find("select").val($(this).data("value"))
+	$(this).parents(".custom-options").find(".custom-option").removeClass("selection")
+	$(this).addClass("selection")
+	$(this).parents(".custom-select").removeClass("opened")
+	$(this).parents(".custom-select").find(".custom-select-trigger").text($(this).text())
 })
